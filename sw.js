b4dabff3457ffe4d110c6a430c5a6dcf64b5fa7f
TobbiChen <chendaiwu123@gmail.com>
2026-08-16 00:11:13 +0800

    Upgrade PWA shell cache to v24

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const C='mango-cards-v22-shell';
+const C='mango-cards-v24-shell';
 const A=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-    const y=x.clone(); caches.open(C).then(c=>c.put(e.request,y)); return x;
-  }).catch(()=>caches.match('./index.html'))));
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(x=>{const y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
